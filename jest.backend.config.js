ebcf824f39aff8d080925c7bc3bfb7d97d92e75f
Muhammad Fahim Tajwar <oscorp4@gmail.com>
2025-10-22 05:07:26 +0800

    Add integrations files to jest backend config

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -8,7 +8,8 @@ export default {
   // which test to run
   testMatch: ["<rootDir>/controllers/*.test.js", "<rootDir>/models/*.test.js",
     "<rootDir>/helpers/*.test.js",
-    "<rootDir>/middlewares/*.test.js"],
+    "<rootDir>/middlewares/*.test.js",
+    "<rootDir>/tests/integration/*.test.js"],
 
   // jest code coverage
   collectCoverage: true,
